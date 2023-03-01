9961c3a70b896b0cc1f93104714753bc3d923144
Giuseppe Suanno <suanno.giuseppe@yahoo.it>
2023-03-01 14:18:59 +0100

    Implementazione Array e id spesa

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const listaSpesa=[
+    'pere',
+    'mele',
+    'banane',
+    'caffè',
+    'carne',
+    'pesce',
+    'pasta',
+    'coca-cola',
+    'birra',
+    'verdure',
+];
+
+const listaDom = document.getElementById('spesa');
+
+
